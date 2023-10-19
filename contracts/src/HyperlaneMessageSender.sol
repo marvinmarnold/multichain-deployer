50cfbb53c0630ac9bedccb0b31389a9b756cec9a
gr8h <hesham.shabana@hotmail.com>
2023-10-19 09:35:41 +0200

    Add supported chains

--- contracts/src/HyperlaneMessageSender.sol
+++ contracts/src/HyperlaneMessageSender.sol
@@ -7,7 +7,9 @@ pragma solidity ^0.8.9;
 import "./IMailbox.sol";
 import "./IInterchainGasPaymaster.sol";
 
-contract HyperlaneMessageSender {
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract HyperlaneMessageSender is Ownable {
     IMailbox outbox;
     IInterchainGasPaymaster igp;
 
@@ -31,6 +33,15 @@ contract HyperlaneMessageSender {
     error InsufficientGas(uint256 required, uint256 provided);
     error DifferentLengthArrays(uint256 lenDomains, uint256 lenGasAmounts);
     error InvalidAddress(address addr);
+    error ChainNotSupported(uint256 chainId);
+
+    // Modifiers
+    modifier equalLengthArrays(uint256 len1, uint256 len2) {
+        if (len1 != len2) {
+            revert DifferentLengthArrays(len1, len2);
+        }
+        _;
+    }
 
     constructor(address _outbox, address _gasPayer) {
         if (_outbox == address(0)) {
@@ -44,26 +55,34 @@ contract HyperlaneMessageSender {
         igp = IInterchainGasPaymaster(_gasPayer);
     }
 
+    function addSupportedChain(uint32 _chainId) external onlyOwner {
+        supportedChains[_chainId] = true;
+    }
+
+    function removeSupportedChain(uint32 _chainId) external onlyOwner {
+        supportedChains[_chainId] = false;
+    }
+
     function getQuote(
         uint32 _destinationDomain,
         uint256 _gasAmount
     ) public view returns (uint256) {
+        if (!supportedChains[_destinationDomain]) {
+            revert ChainNotSupported(_destinationDomain);
+        }
+
         return igp.quoteGasPayment(_destinationDomain, _gasAmount);
     }
 
     function getQuoteForMultipleChains(
         uint32[] calldata _destinationDomain,
         uint256[] calldata _gasAmount
-    ) public view returns (uint256) {
-        if (_destinationDomain.length != _gasAmount.length) {
-            revert DifferentLengthArrays(
-                _destinationDomain.length,
-                _gasAmount.length
-            );
-        }
-
+    ) public view equalLengthArrays returns (uint256) {
         uint256 total = 0;
         for (uint256 i = 0; i < _destinationDomain.length; i++) {
+            if (!supportedChains[_destinationDomain]) {
+                revert ChainNotSupported(_destinationDomain);
+            }
             total += igp.quoteGasPayment(_destinationDomain[i], _gasAmount[i]);
         }
         return total;
@@ -75,6 +94,10 @@ contract HyperlaneMessageSender {
         bytes32 _recipient,
         bytes calldata _message
     ) external payable {
+        if (!supportedChains[_destinationDomain]) {
+            revert ChainNotSupported(_destinationDomain);
+        }
+
         uint256 requiredGas = igp.quoteGasPayment(
             _destinationDomain,
             _gasAmount
@@ -103,14 +126,7 @@ contract HyperlaneMessageSender {
         uint256[] calldata _gasAmount,
         bytes32 _recipient,
         bytes calldata _message
-    ) external payable {
-        if (_destinationDomain.length != _gasAmount.length) {
-            revert DifferentLengthArrays(
-                _destinationDomain.length,
-                _gasAmount.length
-            );
-        }
-
+    ) external payable equalLengthArrays {
         uint256 totalRequiredGas = 0;
         uint256[] memory gasAmountAfterQuote = new uint256[](
             _destinationDomain.length

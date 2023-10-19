af59122c2339aa4299cbc6a73603e8880531d23a
gr8h <hesham.shabana@hotmail.com>
2023-10-19 15:12:08 +0200

    Test Fixes

--- contracts/test/HyperlaneMessageSender.t.sol
+++ contracts/test/HyperlaneMessageSender.t.sol
@@ -5,7 +5,10 @@ import "forge-std/Test.sol";
 import "../src/HyperlaneMessageSender.sol";
 
 contract HyperlaneMessageSenderTest is Test {
-    // the identifiers of the forks
+    address public owner;
+    address public addr1;
+    address public addr2;
+
     uint256 mainnetFork;
     uint256 polygonZkFork;
 
@@ -18,6 +21,10 @@ contract HyperlaneMessageSenderTest is Test {
     address igpAddress = address(0x6cA0B6D22da47f091B7613223cD4BB03a2d77918);
 
     function setUp() public {
+        owner = address(this);
+        addr1 = vm.addr(1);
+        addr2 = vm.addr(2);
+
         mainnetFork = vm.createFork(MAINNET_RPC_URL);
         polygonZkFork = vm.createFork(POLYGONZK_RPC_URL);
     }
@@ -33,14 +40,36 @@ contract HyperlaneMessageSenderTest is Test {
         assertEq(vm.activeFork(), mainnetFork);
     }
 
+    function testSupportedChains() public {
+        vm.selectFork(mainnetFork);
+        assertEq(vm.activeFork(), mainnetFork);
+
+        vm.deal(owner, 10 ether);
+        vm.startPrank(owner);
+
+        senderContract = new HyperlaneMessageSender(mailAddress, igpAddress);
+
+        bytes4 selector = bytes4(keccak256("ChainNotSupported(uint256)"));
+        vm.expectRevert(abi.encodeWithSelector(selector, 1));
+        senderContract.getQuote(1, 200000);
+    }
+
     function testCheckQuote() public {
         vm.selectFork(mainnetFork);
         assertEq(vm.activeFork(), mainnetFork);
 
+        vm.deal(owner, 10 ether);
+        vm.startPrank(owner);
+
         senderContract = new HyperlaneMessageSender(mailAddress, igpAddress);
 
-        uint256 quote = senderContract.getQuote(1, 200000);
+        senderContract.addSupportedChain(1);
+        senderContract.addSupportedChain(137);
 
-        assertGt(quote, 200000, "Quote should be 200000");
+        uint256 quote = senderContract.getQuote(137, 100);
+
+        // emit log_named_uint("quote", quote);
+
+        assertGt(quote, 100, "Quote should be 200000");
     }
 }
